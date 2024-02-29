bb1a355f827567b86f8a13996cd2b004e358c2fd
dvkeia <devikak475@gmail.com>
2024-02-29 17:30:17 -0500

    start of login page and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from "./components/Login/Login"
+import Home from './components/Home/Home';
+
 const App = () => {
-    return (
-      <div>
-        This is our website template.
-      </div>
-    )
+
+  const [token, setToken] = useState(); //token is var for current state, setToken is func to update state
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return(
+  <div>
+  <h1>The Home Page</h1>
+  <BrowserRouter>
+    <Routes>
+      <Route path = "/home" element={<Home />}>
+      </Route>
+      </Routes>
+  </BrowserRouter>
+  </div> 
+  );
 }
 
 export default App
