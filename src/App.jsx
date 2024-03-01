9423e3c69d91056b97cd2e3250522a53b29864c2
dvkeia <devikak475@gmail.com>
2024-03-01 11:50:06 -0500

    Add signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,24 @@ import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from "./components/Login/Login"
 import Home from './components/Home/Home';
+import Signup from './components/Signup/Signup';
 
 const App = () => {
 
-  const [token, setToken] = useState(); //token is var for current state, setToken is func to update state
+  // const [token, setToken] = useState(); //token is var for current state, setToken is func to update state
 
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
+  // if(!token) {
+  //   return <Login setToken={setToken} />
+  // }
 
   return(
-  <div>
-  <h1>The Home Page</h1>
-  <BrowserRouter>
-    <Routes>
-      <Route path = "/home" element={<Home />}>
-      </Route>
+    <div>
+      <Routes>
+        <Route path="/" element = {<Login />}></Route>
+        <Route path = "/home" element={<Home />}></Route>
+        <Route path= "/signup" element={<Signup />}></Route>
       </Routes>
-  </BrowserRouter>
-  </div> 
+    </div> 
   );
 }
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function Login() {
   return(
@@ -15,6 +16,12 @@ export default function Login() {
         </label>
         <div>
             <button type="submit">Submit</button>
+        </div>
+        <div>
+          <p>Don't have an account?</p>
+          <Link to="/signup" className='signup-text'>
+            Sign up Here
+          </Link>
         </div>
         </form>
     </div>
