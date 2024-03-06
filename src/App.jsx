a94ec488f196dcf7f851409a6179b833ea25a1d8
Sheng0841 <itsshengliu@gmail.com>
2024-03-06 15:34:36 -0500

    firebase auth, mod login.jsx. app.jsx, main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/authContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "./components/Login/Login"
 import Home from './components/Home/Home';
 import Signup from './components/Signup/Signup';
 import Navbar from './components/Navbar/Navbar';
+
 
 const App = () => {
 
@@ -14,14 +17,14 @@ const App = () => {
   // }
 
   return(
-    <div>
+    <AuthProvider>
       <Navbar />
       <Routes>
         <Route path="/" element = {<Login />}></Route>
         <Route path = "/home" element={<Home />}></Route>
         <Route path= "/signup" element={<Signup />}></Route>
       </Routes>
-    </div> 
+    </AuthProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <BrowserRouter>
+   <Router>
     <App/>
-   </BrowserRouter>
+   </Router>
   </React.StrictMode>,
 )
