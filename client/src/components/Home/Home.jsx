75aa6ae8e124dd33ca77ee7e0fa9eb87f2dc3665
Alvin Wong <alvinwong@ufl.edu>
2024-03-13 20:30:12 -0400

    Revamping home page and navigation bar - not complete

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import './Home.css';
+import food1Image from '../../images/food1.jpg';
+import food2Image from '../../images/food2.jpg';
+import food3Image from '../../images/food3.jpg';
 
-export default function Home() {
-  return(
-    <h2>Welcome to Calorie Counter Buddy!</h2>
-  );
-}
+const Home = () => {
+    return(
+        <div className="home-page">
+
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                    1 of 2
+                    </div>
+                    <div className="col">
+                        <div className="col-md-4">
+                            <img src={food1Image} width="300" height = "300" className="stack-image" alt="Image 1" style={{ transform: 'rotate(-10deg)' }}/>
+                            <img src={food2Image} width="300" height = "300" className="stack-image" alt="Image 2" style={{ transform: 'rotate(10deg)' }}/>
+                            <img src={food3Image}  width="300" height = "300" className="stack-image" alt="Image 3" style={{ transform: 'rotate(5deg)' }}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+};
+
+export default Home;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import GatorLogo from "../../icons/gatorlogo.png"
+import LoginIcon from "../../icons/my-account-login.svg"
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <div className="links">
-            <Link to="/home">Home</Link>
-            {/* <Link to="/account">Account Profile</Link>
-            <Link to="/calories">Calorie Upload</Link> */}
-        </div>
-    </div>
-  )
+    return (
+        <nav className="navbar navbar-expand-sm">
+            <div className="container-fluid">
+
+                {/* Navbar brand */}
+                <Link className="navbar-brand" to="/">
+                    <img src={GatorLogo} alt="Gator Logo" width="90" height="50" className="d-inline-block align-top"/>
+                </Link>
+
+                {/* Navbar links */}
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="navbar-nav mr-auto"> {/* Use mr-auto to push navbar links to the left */}
+                        {/* Popular link */}
+                        <Link className="nav-link" to="/popular">Popular</Link>
+                        {/* Calories link */}
+                        <Link className="nav-link" to="/calorie-counter">Calories</Link>
+                        {/* About Us link */}
+                        <Link className="nav-link" to="/about">About Us</Link>
+                    
+                    </div>
+                </div>
+
+                {/*Stuff on the Right Hand Side */}
+                {/* Login link */}
+                <Link className="login" to="/login">
+                    <img src={LoginIcon} alt="Login Icon" width="30" height="30" className="login d-inline-block align-top"/>
+                </Link>
+            </div>
+        </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
