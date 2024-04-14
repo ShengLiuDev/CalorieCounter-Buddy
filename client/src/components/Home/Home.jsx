6312ea4f1d9f9027bfebef88cd74aa94b69b93b5
Sheng0841 <itsshengliu@gmail.com>
2024-04-14 17:02:31 -0400

    modified sign in and login css, reroute modified

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -9,8 +9,11 @@ const Home = () => {
                 <div className="home-page-banner-icon">
                     <img src={mainHeader} alt="Homepage Banner" className="home-icon" />
                     {/* Place your welcome text outside of the img tag */}
-                    <h2>Welcome to Calorie Counter Buddy!</h2>
-                    <p>We are your one stop shop for your fitness goals.</p>
+                    <div class="homepage-text"> 
+                        Welcome to Calorie Counter Buddy!
+                        <br></br>
+                        We are your one stop shop for your fitness goals.
+                    </div>
                 </div>
             </div>
         </section>
