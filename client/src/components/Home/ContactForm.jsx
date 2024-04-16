6c4aba2a197d1d388700f0ecb76a9f7cb5fe2d13
Sheng0841 <itsshengliu@gmail.com>
2024-04-16 16:32:33 -0400

    added contact email support using emailJS library, form resets now

--- client/src/components/Home/ContactForm.jsx
+++ client/src/components/Home/ContactForm.jsx
@@ -1,70 +1,59 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
+    const formRef = useRef();
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [stateMessage, setStateMessage] = useState(null);
-    console.log("we have not entered the sendEmail portion of the code yet");
+
     const sendEmail = (e) => {
-        console.log("we have entered the sendEmail portion of the code");
-        e.persist();
         e.preventDefault();
         setIsSubmitting(true);
 
-        emailjs
-            .sendForm(
-                process.env.REACT_APP_SERVICE_ID,
-                process.env.REACT_APP_TEMPLATE_ID,
-                e.target,
-                process.env.REACT_APP_PUBLIC_KEY
-            )
-            .then(
-                (result) => {
-                    setStateMessage('Message Sent!');
-                    setIsSubmitting(false);
-                    setTimeout(() => {
-                        setStateMessage(null);
-                    }, 5000); // hides the message after 5 seconds
-                },
-                (error) => {
-                    setStateMessage('Something went wrong, please try again later');
-                    setIsSubmitting(false);
-                    setTimeout(() => {
-                        setStateMessage(null);
-                    }, 5000); // same functinality of hiding message
-                }
-            );
-        e.target.reset();
-    }
-    
+        // Here we pass the form element reference
+        emailjs.sendForm(
+            'service_iebftmo',
+            'template_vr1floi',
+            formRef.current, // Pass the form reference here
+            'YTk-1A44ReHaqWXnX'
+        ).then((response) => {
+            setStateMessage('Message Sent!');
+            setIsSubmitting(false);
+            setTimeout(() => {
+                setStateMessage(null);
+            }, 5000);
+        }, (error) => {
+            setStateMessage('Something went wrong, please try again later.');
+            setIsSubmitting(false);
+            setTimeout(() => {
+                setStateMessage(null);
+            }, 5000);
+        });
+
+        // Reset form fields after submission
+        formRef.current.reset();
+    };
+
     return (
         <div className='contact-us-header'>
             Contact Us
             <hr></hr>
-            <div className='contact-us-subtext'>
-            <br></br>
-                Need to get in touch with us? Either fill out this form 
-                or send us an email at <space></space> 
-                <span className='our-email'>
-                    caloriecounterbuddy@gmail.com
-                </span>
-            </div>
             <div className='contact-us-information'>
-                <form onSubmit={sendEmail} className='contact-us-form'>
+                <form ref={formRef} onSubmit={sendEmail} className='contact-us-form'>
                     <label>
                         First Name:
-                        <input type="text" firstName="first-name"/>
+                        <input type="text" name="first_name" />
                     </label>
                     <label>
                         Last Name:
-                        <input type="text" lastName="last-name"/>
+                        <input type="text" name="last_name" />
                     </label>
                     <label> 
                         Email:
-                        <input type="text" email="email"/>
+                        <input type="text" name="email" />
                     </label>
                     <label>
-                        Message
+                        Message:
                         <textarea name="message" className='message-box'/>
                     </label>
                     <input type="submit" value="Send" disabled={isSubmitting} />
@@ -72,7 +61,7 @@ const ContactForm = () => {
                 </form>
             </div>
         </div>
-    )
+    );
 };
 
 export default ContactForm;
