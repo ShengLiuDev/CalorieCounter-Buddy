6d390a5a19f5d16049095f237149d81dd7b36530
Sheng Liu <shengliu@ufl.edu>
2024-04-21 18:58:22 -0400

    test

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -11,10 +11,23 @@ const Profile = () => {
         <section>
             <div className="profile-container">
                 <div className="profile-header-text">
-                    <img src={alvin}>
-
-                    </img>
+                    Welcome to Your Profile
+                    <hr></hr>
                 </div>
+                <div className="vertical-line">
+                    
+                </div>
+            </div>
+            <div className="profile-options-container">
+                <Link className="personal-information">
+
+                </Link>
+                <Link className="calorie-count">
+
+                </Link>
+                <Link className="upload-recipe">
+
+                </Link>
             </div>
         </section>
     );
