388da0c4594280d24ce0de581be89ee5a37e571a
Lorraine <lorraine.nix.0@gmail.com>
2024-04-21 21:54:27 -0400

    Added search page functionality

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -4,13 +4,55 @@ import './Search.css';
 
 const Search = () => {
 
+
+    const formReducer = (state, event) => {
+        if(event.reset) {
+            return {
+              SearchText: ''
+            }
+          }
+        return {
+          ...state,
+          [event.name]: event.value
+        }
+    }
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const updatedFormData = {
+            ...formData
+        };
+
+        const searchResponse = await axios.get(`http://localhost:3001/api/recipes/search`, {
+            params: {
+                recipename: updatedFormData.searchText
+            }
+        })
+        setSearchResults(searchResponse.data.recipes.map(res => res));
+    }
+
+    const handleChange = event => {
+        setFormData({
+          name: event.target.name,
+          value: event.target.value,
+        });
+    }
+
     return(
         <div className='wrapper'>
-            <form>
+            <form onSubmit={handleSubmit}>
             <p> Search recipes </p>
-            <input name="search"/>
+            <input name="searchText" onChange={handleChange}/>
             <button>Search</button>
             </form>
+            <div className='searchResults'>
+                {
+                    searchResults.map(result => <div><a href={`http://localhost:5173/recipe/${result.title}`}>{result.title}</a></div>)
+                }
+            </div>
         </div>
     )
 };
